cc7f3adc6aad868d294c825c6497e94fbba4c094
Muhamamd Salif <muhammad.salif@yahoo.com>
2020-12-13 14:57:23 +0500

    Api implemented.

--- app.js
+++ app.js
@@ -6,23 +6,20 @@ const port = process.env.PORT || 5000
 
 app.use(bodyParser.json());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/random/:max/:min", (req, res) => {
+    let max = parseInt(req.params.max)
+    let min = parseInt(req.params.min)
+    if (isNaN(max) || isNaN(min)) {
+        res.status(400).json({
+            error: "Bad request"
+        })
+    }
+    // Random number formula
+    var result = Math.round((Math.random() * (max - min)) + min);
+    res.json({
+        result:result
+    })
+})
 
 app.listen(port, () => {
     console.log('Express app is listening at the http://localhost:', port)
